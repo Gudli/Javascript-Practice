fc10118e4f1238913bbd552f1bd4b0a15faf799c
Sagarika Padhy <88076571+Gudli@users.noreply.github.com>
2024-01-17 11:00:05 +0000

    variables and datatypes

--- Basics/Variables.js
+++ Basics/Variables.js
@@ -1,8 +1,31 @@
+"use strict"; // treat all js code as newer version
+
 const accountId =144553 //cannot change
 let accountEmail  = "sagarika@gmail.com"
 var accountPassword = "12345"
 //can declare variable using let and var. In Var there is issur with scope.so let is used
 
+let x// give undefined
 
-accountcity = "Blr"
-console.table([accountId,accountEmail,accountPassword,accountPassword]);
+console.table([accountId,accountEmail,accountPassword,accountPassword,x]);
+
+//DataTypes
+//alert()//This will throw an error as we are using node js not browser
+let name = "Sagarika"
+let age = 24
+let isLoggedIn = false
+
+//number =>2 to power53
+//bigint
+//string => ""
+//boolean=>true/false
+//null =>standalone vale
+//undefined => value is not assigned
+//symbol =>to fined the uniqueness
+
+//Objects
+
+console.log(typeof age)//number
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+
