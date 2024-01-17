966450822f63c09a982c1b1090a8185da97b2124
Sagarika Padhy <88076571+Gudli@users.noreply.github.com>
2024-01-17 10:22:01 +0000

    changes done

---
+++ Basics/Variables.js
@@ -0,0 +1,8 @@
+const accountId =144553 //cannot change
+let accountEmail  = "sagarika@gmail.com"
+var accountPassword = "12345"
+//can declare variable using let and var. In Var there is issur with scope.so let is used
+
+
+accountcity = "Blr"
+console.table([accountId,accountEmail,accountPassword,accountPassword])
