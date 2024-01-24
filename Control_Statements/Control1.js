fc7a7708f38c1b12b9e1021c7d8c0e207b711239
Sagarika Padhy <88076571+Gudli@users.noreply.github.com>
2024-01-24 23:21:25 +0530

    Add files via upload

---
+++ Control_Statements/Control1.js
@@ -0,0 +1,51 @@
+//if
+//const isUserloggedIn =true
+/*if (2=="2") {
+    console.log("Executed");
+}
+if (2==="2") {
+    console.log("Executed2");
+}*/
+
+//<, >,<=,>=,==,!=,===,!==
+/*const temp =41
+if (temp <50 ) {
+    console.log("less then 50");
+} else {
+    console.log("tempt greater then 50");
+}*/
+/*const score =200
+if (score > 100){
+    const power = "fly"
+    console.log((`User Power: ${power}`));
+}*/
+
+const balance =1000
+/*if (balance >500) console.log("test"); 
+console.log("test2");*/
+
+/*if (balance < 500){
+    console.log("less then 500");}
+    else if (balance<750) {
+        console.log("less then 750");
+    }
+   else if(balance<900){
+    console.log("less then 900");
+   }
+else{
+    console.log("less then 1200");
+}*/
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromgoogle = false
+const loggedInFromEmail =true
+
+if (userLoggedIn && debitCard) {
+    console.log("Allow to buy cource");
+    
+}
+if (loggedInFromgoogle || loggedInFromEmail) {
+    console.log("user logged in");
+}
+
---
+++ Control_Statements/control2.js
@@ -0,0 +1,32 @@
+//switch
+/*switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}*/
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+        case 2 :
+            console.log("Feb");
+            break;
+        case 3:
+            console.log("Mar");
+            break;
+        case 4:
+            console.log("apr");
+            break;
+        case 5:
+            console.log("may");
+            break;
+
+    default:
+        console.log("defult");
+        break;
+}
